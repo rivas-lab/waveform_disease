157af9089e3ff6e9ca23b0be9b2b19f6647551db
salmazainana <szainana@stanford.edu>
2025-08-13 13:00:40 -0700

    cleaned of file not relevent to work in the paper

--- ipca633_dedup.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/scratch/groups/mrivas/mrivasfinal/ipca633.phe", sep='\t')
-df = df.drop_duplicates(subset=["#FID", "IID"])
-df.to_csv("/scratch/groups/mrivas/mrivasfinal/ipca633_dedup.phe", sep='\t', index=False)
